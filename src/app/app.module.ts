e76f8a9482eaf9d95068d4f26fb279eb9f8914ea
jarihant3009 <arihantjain.min18@itbhu.ac.in>
2022-06-08 11:01:53 +0530

    basic loyout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material/material.module';
+import { LeftbarComponent } from './leftbar/leftbar.component';
+import { MiddlebarComponent } from './middlebar/middlebar.component';
+import { RightbarComponent } from './rightbar/rightbar.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LeftbarComponent,
+    MiddlebarComponent,
+    RightbarComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+ 
---
+++ src/app/middlebar/middlebar.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import {FormControl} from '@angular/forms';
+
+
+
+@Component({
+  selector: 'app-middlebar',
+  templateUrl: './middlebar.component.html',
+  styleUrls: ['./middlebar.component.css']
+})
+export class MiddlebarComponent implements OnInit {
+
+  tabs = ['First', 'Second', 'Third'];
+  selected = new FormControl(0);
+
+  addTab(selectAfterAdding: boolean) {
+    this.tabs.push('New');
+
+    if (selectAfterAdding) {
+      this.selected.setValue(this.tabs.length - 1);
+    }
+  }
+
+  removeTab(index: number) {
+    this.tabs.splice(index, 1);
+  }
+  ngOnInit(): void {
+  }
+
+}
+  
